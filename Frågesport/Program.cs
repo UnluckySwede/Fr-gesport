47d44b734dc89dbb1db0d57c09f400ef206bb115
UnluckySwede <gadler.michael@elev.ga.ntig.se>
2021-10-26 14:57:54 +0200

    jurt9iujrt9uij

---
+++ Frågesport/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Frågesport
+{
+    class Program
+    {
+        
+            
+        
+        static void Main(string[] args)
+        {
+        int Points = 0;
+
+        String Answer;
+        
+        String Correct;
+
+        Correct = "ph2";
+        
+        Answer = "ph";
+        
+        Correct = "b";
+        
+            Console.WriteLine("What is the best pet? a) Dog  b) Cat or c) Hamster");
+            Answer = Console.ReadLine();
+            if (Answer == Correct)
+            {
+                Points ++;
+                Answer = "ph";
+                Correct = "a";
+                System.Console.WriteLine("What is the first letter in the Alphabet? a) a b) ö c) oe");
+                Answer = Console.ReadLine();
+                if (Answer == Correct)
+                {
+                    Points ++;
+                    Answer = "ph";
+                    Correct = "";
+                    System.Console.WriteLine("What is the worlds biggest city? a) New York b) Stockholm c) Tokyo");
+                    Answer = Console.ReadLine();
+                    if ()
+                    {
+                        
+                    }
+                }
+                Console.ReadLine();
+
+            }
+            else{
+
+            }
+        }
+    }
+}
